6978529a762640acf4fa1c500c4eb6571b42946a
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-04-18 20:35:39 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesFilmes/Modelo/Filme.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesFilmes.Modelo
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Máximo de 100 Caracteres!")]
+       
+        public string Titulo { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+
+        
+        [Required(ErrorMessage ="Digite o Gênero do Filme!")]
+        [StringLength(30, MinimumLength = 3)]
+        public string Genero { get; set; } = string.Empty;
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18x2)*")]
+        public decimal Preco { get; set; }
+
+        [Range(0,5)]
+        public int Pontos { get; set; } = 0;
+    }
+}
